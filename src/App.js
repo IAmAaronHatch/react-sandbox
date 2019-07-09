21b254d7667c1fa1ff673c7d57ed16a2ff597ce2
Aaron Hatch <aaron@aarons-mbp.attlocal.net>
2019-07-08 21:44:55 -0700

    successfully delete todos

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
 
   // Delete Todo
   delTodo = (id) => {
-    console.log(id)
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
   }
 
   render() {
