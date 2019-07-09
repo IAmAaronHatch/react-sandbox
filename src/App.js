e9919bee60096d83d05418ad71f6ac5111b55732
Aaron Hatch <aaron@Aarons-MacBook-Pro.local>
2019-07-09 14:06:49 -0700

    imported reactrouter, wrapped todos and addtodo in route, render props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// Components
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
+
+// CSS
 import './App.css';
+
+// Extra
 import uuid from 'uuid';
+
 
 
 class App extends Component {
@@ -50,26 +58,34 @@ class App extends Component {
       title,
       completed: false
     }
-    this.setState({ todos: [...this.state.todos, newTodo]})
+    this.setState({ todos: [...this.state.todos, newTodo] })
 
   }
 
 
   render() {
     return (
-      <div>
-        <div className="container">
-          <Header />
-          <AddTodo 
-            addTodo={this.addTodo}
-          />
-          <Todos
-            todos={this.state.todos}
-            markComplete={this.markComplete}
-            delTodo={this.delTodo}
-          />
+      <Router>
+        <div>
+          <div className="container">
+            <Header />
+            <Route path="/" render={props => (
+              <React.Fragment>
+                <AddTodo
+                  addTodo={this.addTodo}
+                />
+                <Todos
+                  todos={this.state.todos}
+                  markComplete={this.markComplete}
+                  delTodo={this.delTodo}
+                />
+              </React.Fragment>
+            )}
+            />
+
+          </div>
         </div>
-      </div>
+      </Router>
     )
   }
 }
